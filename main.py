2a158aa7c0ecc1f5947af4d18ce794504bb0d607
rsemente <rodrigossemente@gmail.com>
2025-03-16 12:18:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import flet as ft
 
+
 def main(page: ft.Page):
-    page.title = "ARKHI 1"
+    page.title = "Flet counter example"
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
@@ -17,12 +19,13 @@ def main(page: ft.Page):
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
                 txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
     )
+
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
